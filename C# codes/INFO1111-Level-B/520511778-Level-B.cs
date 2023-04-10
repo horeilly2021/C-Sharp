ff3f03d1603e341f8c13822868def0db66514826
horeilly2021 <130267537+horeilly2021@users.noreply.github.com>
2023-04-10 20:19:29 +1000

    Add files via upload

---
+++ C# codes/INFO1111-Level-B/520511778-Level-B.cs
@@ -0,0 +1,160 @@
+﻿namespace Attempt_3;
+class Wizard
+{
+    public string name;
+    public string favSpell;
+    public int spellSlots;
+    public float exp;
+    public float health;
+    public int attackDamage;
+
+    public static int Count;
+
+    public Wizard(string _name, string _favSpell)
+    {
+        name = _name;
+        favSpell = _favSpell;
+        spellSlots = 2;
+        exp = 0f;
+        health = 10f;
+        attackDamage = 1;
+
+        Count++;
+    }
+    
+    public void CastSpell(Wizard target)
+    {
+        // using an attack will increase xp by 0.25 and decrease the other wizard by the attack damage
+        if (spellSlots > 0) {
+            Console.WriteLine(favSpell + " does " + attackDamage + " damage");
+            Console.WriteLine(name + " casts " + favSpell);
+            spellSlots--;
+            exp += 0.25f;
+            attackDamage = (Convert.ToInt32(exp) + 1);
+            target.health -= attackDamage;
+
+        } else {
+            Console.WriteLine(name + " is out of spell slots.");
+        }
+        
+    }
+
+    public void Meditate()
+    {
+        // meditating will icnrease the wizards xp by 0.5 and their health by 1
+        Console.WriteLine(name + " meditates to regain spell slots.");
+        exp += 0.5f;
+        spellSlots++;
+        health += 1;
+    }
+
+    public void Flee()
+    {
+        // choosing to flee will end the game
+        Console.WriteLine(name + " has chosen to flee!");
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+
+        // Asking for names and favourite spells
+        Console.Write("What is your name wizard? ");
+        string wizName01 = Console.ReadLine();
+        Console.Write("What is your wizards favourite spell " + wizName01 + "? ");
+        string wizSpell01 = Console.ReadLine();
+        Console.WriteLine("Excellent choice " + wizName01 + ", the " + wizSpell01 + " spell is extremely powerful!");
+
+        Console.Write("Oh your fierce rival has arrived, what is your name challenger? ");
+        string wizName02 = Console.ReadLine();
+        Console.Write("And what the spell you will challenge " + wizName01 + " with " + wizName02 + "? ");
+        string wizSpell02 = Console.ReadLine();
+        Console.WriteLine("Excellent choice " + wizName02 + ", the " + wizSpell02 + " spell is nasty counterspell to " + wizSpell01 + "!");
+
+        // Defining wizard 1 and wizard 2
+        Wizard wizard01 = new Wizard(wizName01, wizSpell01);
+
+        Wizard wizard02 = new Wizard(wizName02, wizSpell02);
+
+        // Asking if the two wizards want to fight
+        Console.Write(wizard01.name + " would you like to engage " + wizard02.name + " in combat, yes or no? ");
+        string combatWiz01 = Console.ReadLine();
+        if (combatWiz01 == "yes") {
+            Console.WriteLine(wizard02.name + " now is your final chance to walk away. Do you wish to engage with " + wizard01.name + ", yes or no.");
+            string combatWiz02 = Console.ReadLine();
+            if (combatWiz02 == "yes") {
+                Console.WriteLine("Ahhh, this should be interesting. DRAW! YOUR! WANDS!!!");
+            } else {
+                Console.WriteLine("Wise choice " + wizard02.name + ", " + wizard01.name + " has a very powerful energy that I can sense.");
+                Environment.Exit(0);
+            }
+        } else {
+            Console.WriteLine("Wise choice " + wizard01.name + ", " + wizard02.name + " has a very powerful energy that I can sense.");
+            Environment.Exit(0);
+        }
+
+        Console.WriteLine(wizard01.name + " since you were here first, you will start.");
+
+        while (true) 
+        {
+            // Wizard one's turn
+            Console.WriteLine(wizard01.name + "'s turn:");
+            Console.WriteLine("Enter 1 to attack, 2 to meditate or 3 to flee:");
+            int choice01 = Convert.ToInt32((Console.ReadLine()));
+
+            if (choice01 == 1) {
+                wizard01.CastSpell(wizard02);
+                Console.WriteLine(wizard01.name + " has " + wizard01.spellSlots + " spell slots left, " + wizard01.exp + " experience and " + wizard01.health + " health left.");
+                Console.WriteLine(wizard02.name + " has " + wizard02.spellSlots + " spell slots left, " + wizard02.exp + " experience and " + wizard02.health + " health left.");
+            } else if (choice01 == 2) {
+                wizard01.Meditate();
+                Console.WriteLine(wizard01.name + " has " + wizard01.spellSlots + " spell slots left, " + wizard01.exp + " experience and " + wizard01.health + " health left.");
+            } else if (choice01 == 3) {
+                wizard01.Flee();
+                Console.WriteLine(wizard02.name + " wins!");
+                Environment.Exit(0);
+            } else {
+                Console.WriteLine("You have picked neither, you have forfited your turn.");
+            }
+
+            if (wizard02.health <= 0) {
+                Console.WriteLine(wizard01.name + " wins!");
+                Environment.Exit(0);
+            }
+            
+            Console.WriteLine("\n");
+
+            // Wizard two's turn
+            Console.WriteLine(wizard02.name + "'s turn:");
+            Console.WriteLine("Enter 1 to attack, 2 to meditate or 3 to flee:");
+            int choice02 = Convert.ToInt32((Console.ReadLine()));
+
+            if (choice02 == 1) {
+                wizard02.CastSpell(wizard01);
+                Console.WriteLine(wizard02.name + " has " + wizard02.spellSlots + " spell slots left, " + wizard02.exp + " experience and " + wizard02.health + " health left.");
+                Console.WriteLine(wizard02.name + " has " + wizard01.spellSlots + " spell slots left, " + wizard01.exp + " experience and " + wizard01.health + " health left.");
+            } else if (choice02 == 2) {
+                wizard02.Meditate();
+                Console.WriteLine(wizard02.name + " has " + wizard02.spellSlots + " spell slots left, " + wizard02.exp + " experience and " + wizard02.health + " health left.");
+            } else if (choice02 == 3) {
+                wizard02.Flee();
+                Console.WriteLine(wizard01.name + " wins!");
+                Environment.Exit(0);
+            } else {
+                Console.WriteLine("You have picked neither, you have forfited your turn.");
+            }
+
+            if (wizard01.health <= 0) {
+                Console.WriteLine(wizard02.name + " wins!");
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("\n");
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ C# codes/INFO1111-Level_A/520511778-Level-A.cs
@@ -0,0 +1,173 @@
+﻿using System;
+
+namespace attempt_2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine("Welcome to the calculator!");
+
+            while (true) {
+
+                Console.WriteLine("Press 1 to continue or 2 to quit: ");
+                int welcome;
+
+                if (!int.TryParse(Console.ReadLine(), out welcome)) {
+
+                    Console.WriteLine("\r\nInvalid input, please enter a number.");
+                    continue;
+
+                }
+
+                if (welcome == 1) {
+
+                    while (true) {
+
+                        Console.Write("\r\nPlease your first number: ");
+                        double num01;
+
+                        if (!double.TryParse(Console.ReadLine(), out num01)) {
+
+                            Console.WriteLine("\r\nInvalid input, please enter a number.");
+                            continue;
+
+                        }
+
+                        Console.Write("Please your second number: ");
+                        double num02;
+
+                        if (!double.TryParse(Console.ReadLine(), out num02)) {
+
+                            Console.WriteLine("\r\nInvalid input, please enter a number.");
+                            continue;
+                            
+                        }
+
+                        Console.WriteLine("\r\nPlease enter what you would like to do:");
+                        Console.Write("1. Add\r\n2. Subtract\r\n3. Multiply\r\n4. Divide\r\n5. Return to main\r\n");
+                        int choice;
+
+                        if (!int.TryParse(Console.ReadLine(), out choice)) {
+
+                            Console.WriteLine("\r\nInvalid input, please enter a number.");
+                            continue;
+
+                        }
+
+                        if (choice == 1) {
+
+                            double added = num01 + num02;
+                            Console.WriteLine("\r\nThe sum of " + num01 + " and " + num02 + " is " + added + "\r\n");
+                            continue;
+
+                        } else if (choice == 2) {
+
+                            while (true) {
+
+                                Console.WriteLine("\r\nFor the difference between " + num01 + " and " + num02 + " type 1.");
+                                Console.WriteLine("For the difference between " + num02 + " and " + num01 + " type 2.");
+                                int sub_choice;
+
+                                if (!int.TryParse(Console.ReadLine(), out sub_choice)) {
+
+                                    Console.WriteLine("\r\nInvalid input, please enter a number.");
+                                    continue;
+
+                                }
+
+                                if (sub_choice == 1) {
+
+                                    double subtract01 = num01 - num02;
+                                    Console.WriteLine("\r\nThe difference between " + num01 + " and " + num02 + " is: " + subtract01);
+                                    break;
+
+                                } else if (sub_choice == 2) {
+
+                                    double subtract02 = num02 - num01;
+                                    Console.WriteLine("\r\nThe difference between " + num02 + " and " + num01 + " is: " + subtract02);
+                                    break;
+
+                                } else {
+
+                                    Console.WriteLine("\r\nInvalid command, try again.");
+
+                                }
+                            }
+                            continue;
+
+                        } else if (choice == 3) {
+
+                            double multiply = num01 * num02;
+                            Console.WriteLine("\r\nThe product of " + num01 + " and " + num02 + " is " + multiply);
+                            continue;
+
+                        } else if (choice == 4) {
+
+                            while (true) {
+
+                                Console.WriteLine("\r\nFor the quotient between " + num01 + " and " + num02 + " type 1.");
+                                Console.WriteLine("For the quotient between " + num02 + " and " + num01 + " type 2.");
+                                int div_choice;
+
+                                if (!int.TryParse(Console.ReadLine(), out div_choice)) {
+
+                                    Console.WriteLine("\r\nInvalid input, please enter a number.");
+                                    continue;
+
+                                }
+
+                                if (div_choice == 1) {
+
+                                    double quotient01 = num01 / num02;
+                                    Console.WriteLine("The quotient between " + num01 + " and " + num02 + " is: " + quotient01 + "\r\n");
+                                    break;
+
+                                } else if (div_choice == 2) {
+
+                                    double quotient02 = num02 / num01;
+                                    Console.WriteLine("The quotient between " + num02 + " and " + num01 + " is: " + quotient02 + "\r\n");
+                                    break;
+
+                                } else {
+
+                                    Console.WriteLine("Invalid command, try again.");
+
+                                }
+                            }
+                            continue;
+
+                        } else if (choice == 5) {
+
+                            Console.WriteLine("Returning to main\r\n");
+                            break;
+
+                        } else {
+                            
+                            Console.WriteLine("Invalid command, try again.");
+
+                        }
+
+                    }
+
+                } else if (welcome == 2) {
+
+                    Console.WriteLine("Thank you for using the calculator, Goodbye!");
+                    Environment.Exit(0);
+
+                } else {
+
+                    Console.WriteLine("Invalid input, try again");
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
